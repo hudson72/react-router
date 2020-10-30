7809a843aa3f5d8083f7268418e364638ba7f2b8
Irek Iwanca <iiwanca@gmail.com>
2020-10-30 13:19:25 +0100

    Updates

---
+++ precache-manifest.6cd65f928536a3117ef8c1e3cbf110f7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "78d25706033164de19c1",
+    "url": "/react-router/static/css/main.54070122.chunk.css"
+  },
+  {
+    "revision": "78d25706033164de19c1",
+    "url": "/react-router/static/js/main.2f72aa64.chunk.js"
+  },
+  {
+    "revision": "c6984152ca93223c2cb4",
+    "url": "/react-router/static/js/runtime~main.fdc07815.js"
+  },
+  {
+    "revision": "bbd286575f27dcf43b03",
+    "url": "/react-router/static/js/2.4f1c3d8b.chunk.js"
+  },
+  {
+    "revision": "70e6670ee746fc59f414fde4fcce93af",
+    "url": "/react-router/static/media/header1.70e6670e.jpg"
+  },
+  {
+    "revision": "d9d36b65a132d26aed884ad58e4719eb",
+    "url": "/react-router/static/media/header3.d9d36b65.jpg"
+  },
+  {
+    "revision": "33eca35fceea8311d67358a6629fa9d9",
+    "url": "/react-router/static/media/header2.33eca35f.jpg"
+  },
+  {
+    "revision": "a5a05e040db3241db9e36584523447ed",
+    "url": "/react-router/index.html"
+  }
+];
